0d011e7d0d9f910012b4eb5fe47edfce35b28f1a
Surya Chakraborty <suryachakraborty.dev@gmail.com>
2026-05-23 12:08:12 +0530

    updated app.jsx

--- react/architecture/src/App.jsx
+++ react/architecture/src/App.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Blogs from './pages/Blogs'
-import Contact from './pages/Contact'
-import Nopage from './pages/Nopage'
-import Layout from './pages/Layout'
+// import { BrowserRouter, Routes, Route} from 'react-router-dom'
+// import Home from './pages/Home'
+// import Blogs from './pages/Blogs'
+// import Contact from './pages/Contact'
+// import Nopage from './pages/Nopage'
+// import Layout from './pages/Layout'
 import InputFocus from './InputFocus'
 import Chat from './ScrollBottom'
 import Stopwatch from './StopWatch'
+import { useState } from 'react'
+import usePrev from './customHooks/usePrev'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const prevCount = usePrev(count)
   return (
     <>
     <InputFocus/>
@@ -16,7 +20,18 @@ const App = () => {
     <Chat/>
     <br />
     <Stopwatch/>
-      <BrowserRouter>
+
+    <br/>
+    usePrev counter:
+    <div>
+      <p>Current Count: {count}</p>
+      <p>Previous Count: {prevCount}</p>
+      <button onClick={() => setCount(count + 1)}>Increament</button>
+      <button onClick={() => setCount(count - 1)}>Decreament</button>
+    </div>
+
+    
+      {/* <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout/>}>
             <Route index element={<Home/>} />
@@ -25,7 +40,8 @@ const App = () => {
             <Route path='*' element={<Nopage/>}/>
           </Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      
     </>
   )
 }
