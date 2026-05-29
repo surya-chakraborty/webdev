905b755efb911fa76013228d869c894b1d150bbd
Surya Chakraborty <suryachakraborty.dev@gmail.com>
2026-05-29 19:22:18 +0530

    configure prisma with postgres container, migrations, and shared client

---
+++ prismaORM/lib/prisma.ts
@@ -0,0 +1,15 @@
+import "dotenv/config"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../generated/prisma/client"
+
+// create instance of prisma client to use in diffrent files
+// pass in the connection string to prismaPg adapter 
+// and the dapter to the prisma client instance
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
